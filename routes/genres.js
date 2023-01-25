7e1fcb497473b9204fd9e11404548ddb56b36498
Ikenna Udemezue <iykethe1st@gmail.com>
2023-01-25 10:30:31 +0100

    genres fix

--- routes/genres.js
+++ routes/genres.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { Genre, validate } = require("../models/genres");
 
 router.get("/", async (req, res) => {
-  throw new Error("Could not get the genres");
+  // throw new Error("Could not get the genres");
   const genres = await Genre.find().sort("name");
   res.send(genres);
 });
